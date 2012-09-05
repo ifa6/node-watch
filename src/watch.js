bf29a8c3e9524e6034d9dd4b5288b3b75b22c4d8
Jorrit Duin <jorrit.duin@gmail.com>
2012-09-05 17:56:53 +0200

    Ready for npm publish

--- src/watch.js
+++ src/watch.js
@@ -1,7 +1,21 @@
 // [Node-watch](https://github.com/jorritd/node-watch) Is a small [nodejs](http://www.nodejs.org/) 
 // module/lib to watch for file changes.
 // Filechanges are: 
-//
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses>.
+ * 
+ */
 // 1. changes where the mtime (make-time) of file's changes.
 // 2. Files added to a watched folder
 // 3. Files deleted from a watched folder
